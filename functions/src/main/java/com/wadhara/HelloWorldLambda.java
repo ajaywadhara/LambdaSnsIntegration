fc8cd441efbef60ca8bde89787ec5a28ec24e4b7
Ajay Wadhara <ajaywadhara@Ajays-MacBook-Pro.local>
2022-08-18 20:39:18 +0100

    changes

--- functions/src/main/java/com/wadhara/HelloWorldLambda.java
+++ functions/src/main/java/com/wadhara/HelloWorldLambda.java
@@ -3,10 +3,14 @@ package com.wadhara;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.wadhara.model.Employee;
 import com.wadhara.model.Response;
 import lombok.extern.log4j.Log4j2;
 
 import java.net.HttpURLConnection;
+import java.util.Optional;
 
 /**
  * Hello world!
@@ -15,16 +19,17 @@ import java.net.HttpURLConnection;
 @Log4j2
 public class HelloWorldLambda implements RequestHandler<SNSEvent, Response>
 {
-
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     @Override
     public Response handleRequest(SNSEvent event, Context context) {
-        try {
-            //process SNS event
-            event.getRecords()
-                    .forEach(v -> log.info("Message received from SNS:{}",
-                            v.getSNS().getMessage()));
-            //message attributes
-            //event.getRecords().forEach(v -> v.getSNS().getMessageAttributes());
+        try{
+            Optional<SNSEvent.SNSRecord> snsRecord = event.getRecords().stream().findAny();
+            if(snsRecord.isPresent()){
+                final SNSEvent.SNS sns = snsRecord.get().getSNS();
+                final String jsonMessage = sns.getMessage();
+                final Employee employee = gson.fromJson(jsonMessage, Employee.class);
+                log.info("Employee Object:{}", employee.toString());
+            }
             return Response.builder()
                     .httpCode(HttpURLConnection.HTTP_OK)
                     .message("OK.")
--- functions/src/main/java/com/wadhara/model/Employee.java
+++ functions/src/main/java/com/wadhara/model/Employee.java
@@ -1,10 +1,12 @@
 package com.wadhara.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class Employee {
     private Integer empId;
     private String name;
